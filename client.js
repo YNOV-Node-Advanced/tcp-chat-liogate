c913f8ab7fe38f90db05fe17785127558339f2e9
Etienne Fachaux <etienne.fachaux@skyloud.fr>
2018-05-31 12:57:38 +0200

    Server working

---
+++ client.js
@@ -0,0 +1,34 @@
+const net = require('net');
+const readline = require('readline');
+
+
+var client = new net.Socket();
+client.connect(80, '127.0.0.1', function() {
+  console.log('> Connected\n');  // acknowledge socket connection
+  // client.write('Hello, server! Love, Client.\n');
+  startReading();
+});
+client.on('data', function(data) {
+  console.log(data.toString()); // display info received from server
+});
+client.on('close', function() {
+  console.log('< Connection closed');
+});
+client.on('error', (err) => {
+  console.log(err);
+});
+
+function startReading() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.prompt();
+  rl.on('line', (line) => {
+    client.write(line.trim());
+    rl.prompt();
+  }).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
+  });
+}
